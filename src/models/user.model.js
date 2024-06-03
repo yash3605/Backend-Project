4177515fa44ae1e195db11687e56fbef98f8f6d8
YashPK <pratapyashkulshrestha@gmail.com>
2024-06-03 18:55:14 +0530

    user and video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, {Schema} from 'mongoose';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 
 const UserSchema = new Schema({
     username:{
@@ -44,6 +46,37 @@ const UserSchema = new Schema({
 },{
     timestamps: true
 });
+
+UserSchema.pre("save", async function(next) {
+    if(!this.isModified('password')) return next();
+
+    this.password =  bcrypt.hash(this.password, 10);
+    next();
+})
+
+UserSchema.methods.isPasswordCorrect = async function(password) {
+      return await  bcrypt.compare(password, this.password);
+}
+
+UserSchema.methods.generateAccessToken = function() {
+   return  jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullname: this.fullname,
+    }, process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    })
+}
+UserSchema.methods.generateRefreshToken = function() {
+    return  jwt.sign({
+        _id: this._id,
+    }, process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn: process.env.EFRESH_TOKEN_EXPIRY
+    })
+}
 
 
 export const User = mongoose.model('User', UserSchema)
