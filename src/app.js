cf40c0a06c3cbca4abd2107967a0a4939896d113
YashPK <pratapyashkulshrestha@gmail.com>
2024-06-06 18:30:39 +0530

    User registry assembled

--- src/app.js
+++ src/app.js
@@ -15,6 +15,15 @@ app.use(express.static('public'));
 
 app.use(cookieParser());
 
+//Routes import
+
+import userRouter from './routes/user.routes.js';
+
+//routes declaration
+
+app.use("/api/v1/users", userRouter)
+
+
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // require('dotenv').config({path: './env'});
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import {app} from './app.js';
 
 dotenv.config({
     path: './.env'
@@ -13,7 +14,7 @@ connectDB()
     })
 })
 .catch((error) => {
-    console.log("MONGODB connection failed");
+    console.log("MONGODB connection failed",error);
 })
 
 
