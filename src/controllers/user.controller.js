2c23281325d55d0076f5aa8c80687a3eab13ca3b
YashPK <pratapyashkulshrestha@gmail.com>
2024-06-06 22:03:53 +0530

    postman testing of User done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -27,7 +27,7 @@ const registerUser = asyncHandler( async(req,res) => {
         throw new ApiError(400, "All fields are required")  
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{email} , {username}]
     })
 
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,29 +1,38 @@
-import {v2 as cloudinary} from 'cloudinary';
-import fs from 'fs';
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+import dotenv from "dotenv"
 
+dotenv.config({
+    path: "./.env"
+})
 
 
 cloudinary.config({ 
-        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-        api_key: process.env.CLOUDINARY_API_KEY, 
-        api_secret: process.env.CLOUDINARY_API_SECRET
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
     try {
-        if (!localFilePath) {
-            return null
-        }
-        const response= await cloudinary.uploader.upload(localFilePath, {
-            resource_type: "auto",
+        if (!localFilePath) return null
+        //upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
         })
-     console.log("File uploaded successfully", response.url);
-     return response;
+        // file has been uploaded successfull
+        //console.log("file is uploaded on cloudinary ", response.url);
+        //fs.unlinkSync(localFilePath)
+        return response;
+
     } catch (error) {
-        fs.unlinkSync(localFilePath); //remove the local temporary saved file as operation get failed
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+        console.log("Error while uploading file on cloudinary ", error);
         return null;
+        
     }
-};
+}
+
 
 
 export {uploadOnCloudinary}
