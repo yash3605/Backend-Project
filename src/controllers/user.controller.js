1ae4a886a25b57bc3cdf11c6d0a7dab33b1b6110
YashPK <pratapyashkulshrestha@gmail.com>
2024-06-07 16:38:49 +0530

    done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -16,7 +16,6 @@ const registerUser = asyncHandler( async(req,res) => {
     // return response
 
     const {fullname, email, username, password} = req.body
-    console.log("email:", email);
 
     // if(fullname == ""){
     //     throw new ApiError(400, "Fullname is required")
@@ -36,7 +35,12 @@ const registerUser = asyncHandler( async(req,res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar is required")
