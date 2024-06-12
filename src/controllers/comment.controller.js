a0c061f88c73f73708808442d1bb757a53e7ad20
YashPK <pratapyashkulshrestha@gmail.com>
2024-06-12 20:27:29 +0530

    feat: Implement like and comment functionality for videos, comments, and tweets

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
 import {Comment} from "../models/comment.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -8,18 +8,146 @@ const getVideoComments = asyncHandler(async (req, res) => {
     // TODO: get all comments for a video
     const {videoId} = req.params;
     const {page = 1, limit = 10} = req.query;
+
+    if(!videoId || isValidObjectId(videoId)){
+        throw new ApiError(400, "Invalid video id");
+    }
+
+    page = isNaN(page) ? 1 : Number(page);
+    limit = isNaN(limit) ? 10 : Number(limit);
+
+    const videoComments = await Comment.aggregate([
+        {
+            $match: {
+                video: mongoose.Types.ObjectId(videoId)
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "commentedBy",
+                foreignField: "_id",
+                as: "commentedBy",
+                pipeline: [
+                    {
+                        $project: {
+                            username: 1,
+                            fullname: 1,
+                            avatar: 1
+                        },
+                    },
+                ]
+            }
+        },
+        {
+            $addFields: {
+                commentBy: {
+                    $first: "$commentedBy"
+                }
+            }
+        },
+        {
+            $skip: (page - 1) * limit
+        },
+        {
+            $limit: limit
+        }
+        
+    ])
+
+    res
+    .status(200)
+    .json(new ApiResponse(200, videoComments, "Comments fetched successfully"));
 });
 
 const addComment = asyncHandler(async (req, res) => {
     //TODO: add a comment to a video
+    const {videoId} = req.params;
+    const {content} = req.body;
+
+    if(!videoId || !isValidObjectId(videoId)){
+        throw new ApiError(400, "Invalid Video ID");
+    }
+
+    if(!content || content.trim() === ""){
+        throw new ApiError(400, "Comment content is required");
+    }
+
+    const video = await Video.findById(videoId);
+
+    if(!video){
+        throw new ApiError(400, "Video doesn't exist");
+    }
+
+    const userComment = Comment.create({
+        video: videoId,
+        content: content.trim(),
+        commentedBy: req.user?._id
+    });
+
+    if(!userComment){
+        throw new ApiError(500, "Failed to add comment");
+    }
+
+    res
+    .status(201)
+    .json(new ApiResponse(201, userComment, "Comment added successfully"));
+
+
 });
 
 const updateComment = asyncHandler(async (req, res) => {
     //TODO: update a comment
+    const {commentId} = req.params;
+    const {content} = req.body;
+
+    if(!commentId || !isValidObjectId(commentId)){
+        throw new ApiError(400, "Invalid Comment ID");
+    }
+
+    if(!content || content.trim() === ""){
+        throw new ApiError(400, "Comment content is required");
+    }
+
+    const updatedComment = await Comment.findByIdAndUpdate(commentId,
+      {
+           $set: {
+              content: content.trim()
+           }
+      },
+      {
+           new: true
+      }
+  );
+
+    if(!updatedComment){
+        throw new ApiError(500, "Failed to update comment");
+    }
+
+    res
+    .status(200)
+    .json(new ApiResponse(200, updatedComment, "Comment updated successfully"));
+
+
 });
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
+    const {commentId} = req.params;
+
+    if(!commentId || !isValidObjectId(commentId)){
+        throw new ApiError(400, "Invalid Comment ID");
+    }
+
+    const deletedComment = await Comment.findByIdAndDelete(commentId);
+
+    if(!deletedComment){
+        throw new ApiError(500, "Failed to delete comment");
+    }
+
+    res
+    .status(200)
+    .json(new ApiResponse(200, null, "Comment deleted successfully"));
 });
 
 export { getVideoComments, addComment, updateComment, deleteComment };
--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -7,20 +7,146 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const { videoId } = req.params;
     // TODO: toggle like for a video
+
+    const userId = req.user._id;
+
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid video id");
+    }
+
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400, "Invalid user id");
+    }
+
+    const isLike = await Like.findOne({ video: videoId, likedBy: userId });
+
+    if (isLike) {
+        await Like.findByIdAndDelete(isLike._id);
+        return res
+            .status(200)
+            .json(new ApiResponse(200, null, "Like removed successfully"));
+    }
+
+    const like = Like.create({
+        video: videoId,
+        likedBy: userId,
+    
+    });
+
+    res
+        .status(201)
+        .json(new ApiResponse(201, like, "Video liked successfully"));
 })
 
 const toogleCommentLike = asyncHandler(async (req, res) => {
     const { commentId } = req.params;
     // TODO: toggle like for a comment
+
+    const userId = req.user._id;
+
+    if (!isValidObjectId(commentId)) {
+        throw new ApiError(400, "Invalid comment id");
+    }
+
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400, "Invalid user id");
+    }
+
+    const isLike = await Like.findOne({ comment: commentId, likedBy: userId });
+
+    if (isLike) {
+        await Like.findByIdAndDelete(isLike._id);
+        return res
+            .status(200)
+            .json(new ApiResponse(200, null, "Like removed successfully"));
+    }
+
+    const like = Like.create({
+        comment: commentId,
+        likedBy: userId,
+    
+    });
+
+    res
+        .status(201)
+        .json(new ApiResponse(201, like, "Comment liked successfully"));
 });
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const { tweetId } = req.params;
     // TODO: toggle like for a tweet
+
+    const userId = req.user._id;
+
+    if (!isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid tweet id");
+    }
+
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400, "Invalid user id");
+    }
+
+    const isLike = await Like.findOne({ tweet: tweetId, likedBy: userId });
+
+    if (isLike) {
+        await Like.findByIdAndDelete(isLike._id);
+        return res
+            .status(200)
+            .json(new ApiResponse(200, null, "Like removed successfully"));
+    }
+
+    const like = Like.create({
+        tweet: tweetId,
+        likedBy: userId,
+    
+    });
+
+    res
+        .status(201)
+        .json(new ApiResponse(201, like, "Tweet liked successfully"));
 });
 
 const getLikedVideos = asyncHandler(async (req, res) => {
     // TODO: get all liked videos
+
+    const userId = req.user._id;
+
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400, "Invalid user id");
+    }
+
+    const likedVideos = await Like.aggregate([
+        {
+            $match: {
+                likedBy: userId,
+                video: { $exists: true }
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "video",
+                foreignField: "_id",
+                as: "allVideos"
+            }
+        },
+        {
+            $unwind: "$allVideos"
+        },
+        {
+            $project: {
+                id: "$allVideos._id",
+                title: "$allVideos.title",
+                description: "$allVideos.description",
+                owner: "$allVideos.owner",
+                createdAt: "$allVideos.createdAt",
+            }
+        }
+    ]);
+
+    res 
+    .status(200)
+    .json(new ApiResponse(200, likedVideos, "Liked videos fetched successfully"));
 })
 
 export { toggleVideoLike, toogleCommentLike, toggleTweetLike, getLikedVideos };
